378f462b27773242a0e94df58e4d0b3605cd2a6d
vagrant <vagrant@localhost.localdomain>
2020-07-31 14:02:58 +0000

    :tada:

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,11 +6,15 @@
 
 package 'httpd'
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-           <h2>ipaddress: #{node['ipaddress']}</h2>
-           <h2>hostname: #{node['hostname']}</h2>"
-end 
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+end
+
+# file '/var/www/html/index.html' do
+#  content "<h1>Hello, world!</h1>
+#        <h2>ipaddress: #{node['ipaddress']}</h2>
+#           <h2>hostname: #{node['hostname']}</h2>"
+#end 
 
 service 'httpd' do
   action [:start, :enable]
